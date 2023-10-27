21b4ae7c6a6316c72f0c40301c18d8e0fc9ba029
lordburaa <comrdebiruk97@gmail.com>
2023-10-27 00:22:06 -0700

    1.3

--- handle_print.c
+++ handle_print.c
@@ -8,23 +8,22 @@
  */
 int handle_print(const char *format, int *index, va_list list)
 {
-	int i = 0;
+	int len_unknow, i;
 	int printed_chars = -1;
 	format_t fmt_types = {
 		{'c', print_char}, {'s', print_string},
-		{'%', print_percent}};
+		{'%', print_percent}, {'\0', NULL}};
 	for (i = 0; fmt_types.format != '\0'; i++)
-	{
 		if (fmt[*ind] == fmt_types[i].fmt)
 			return (fmt_types[i].ptr(list));
-		if (fmt_typed[i].format == '\0')
-		{
-			if (fmt[*index] == '\0')
-				return (-1);
-		}
-
+	if (fmt_typed[i].format == '\0')
+	{
+		if (fmt[*index] == '\0')
+			return (-1);
+		if(format[*index - 1] == ' ')
+			len_unknow += write(1, " ", 1);
+		len_unknow += write(1, &format[*index], 1);
+		return (len_unknow);
 	}
-
-return (printed_chars);
-
+	return (printed_chars);
 }
