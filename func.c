83c10c5bf41daee4ffaa78ec3d7e9b5e6884ad13
lordburaa <comrdebiruk97@gmail.com>
2023-11-25 00:33:17 -0800

    a;;2

--- func.c
+++ func.c
@@ -93,8 +93,10 @@ int print_binary(va_list types, char buffer[], int *ind)
 	for (i = 0, sum = 0, count = 0; i < 32; i++)
 	{
 		sum += a[i];
+		buffer[(*ind)] = '0' + 0;
 		if (sum)
 		{
+			++(*ind);
 			buffer[(*ind)] = a[i] + '0';
 			++(*ind);
 			count++;
